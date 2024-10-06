3b54f45b37d3a6f81d8c4623bac0b44ff3ff1657
anirudh-singh01 <anirudhsingh.ani321@gmail.com>
2024-10-06 21:48:11 +0530

    adding files

---
+++ client/src/components/common/Footer.tsx
@@ -0,0 +1,17 @@
+function Footer() {
+    return (
+        <footer className="static bottom-1 left-0 flex w-full justify-center sm:fixed">
+            <span>
+                Build with ❤️ by{" "}
+                <a
+                    href="https://github.com/anirudh-singh01"
+                    className="text-primary underline underline-offset-1"
+                >
+                    Anirudh Singh
+                </a>
+            </span>
+        </footer>
+    )
+}
+
+export default Footer
